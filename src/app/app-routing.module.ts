739b2d87d98b5f43ed49be0bb1f61e914e6f5cbf
Randa-10 <randa010011012@gmail.com>
2023-11-21 13:50:52 +0200

    get products and edit on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,22 +13,28 @@ import { OrdersComponent } from './components/orders/orders.component';
 
 const routes: Routes = [
   {
+    path: 'Users',
+    loadChildren: () => import('./components/user-auth/user-auth.module').then(m => m.UserAuthModule)
+  },
+  { path: '', redirectTo: 'Users/login', pathMatch: 'full' },
+  {
     path: 'home',
     component: HomeComponent,
     children: [
-      { path: '', redirectTo: '/home/dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent, title: 'dashboard page',canActivate:[UserAuthGuard] },
-      { path: 'sales', component: SalesComponent, title: 'sales page',canActivate:[UserAuthGuard]  },
-      { path: 'products', component: ProductsComponent, title: 'products page' },
-      { path: 'users', component: UsersComponent, title: 'users page' },
-      { path: 'orders', component: OrdersComponent, title: 'orders page' },
-      // {
+      { path: 'dashboard', component: DashboardComponent, title: 'dashboard page',canActivate: [UserAuthGuard]  },
+      { path: 'sales', component: SalesComponent, title: 'sales page', canActivate: [UserAuthGuard] },
+      { path: 'products', component: ProductsComponent, title: 'products page',canActivate: [UserAuthGuard]  },
+      { path: 'users', component: UsersComponent, title: 'users page',canActivate: [UserAuthGuard]  },
+      { path: 'orders', component: OrdersComponent, title: 'orders page' ,canActivate: [UserAuthGuard]} 
+      // ,{
       //   path: 'Users',
       //   loadChildren: () => import('./components/user-auth/user-auth.module').then(m => m.UserAuthModule)
-      // }
+      // },
+      ,{ path: '**', redirectTo: 'Users/login', pathMatch: 'full' } 
     ]
   }
 ];
+
 
 
 @NgModule({
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Products } from 'src/app/models/products';
+import { ProductService } from 'src/app/services/product.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
 
+
+export class DashboardComponent  implements OnInit {
+  products: Products[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void {
+    this.productService.getProducts().subscribe(
+      (response) => {
+        this.handleApiResponse(response);
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+        // Handle error (display message, etc.)
+      }
+    );
+  }
+
+  private handleApiResponse(response: any): void {
+    if (response.message === 'Products fetched successfully' && Array.isArray(response.data)) {
+      this.products = response.data;
+    } else {
+      console.error('Invalid API response:', response);
+      // Handle unexpected API response
+    }
+  }
+
+  // deleteProduct(productId: number): void {
+  //   // Implement the logic to delete the product here
+  //   this.productService.deleteProduct(productId).subscribe(
+  //     (data) => {
+  //       // Handle successful deletion, if needed
+  //     },
+  //     (error) => {
+  //       console.error('Error deleting product:', error);
+  //       // Handle error (display message, etc.)
+  //     }
+  //   );
+
+// }}
 }
+ 
